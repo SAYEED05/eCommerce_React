e37c510f89bcb981de91ad557da6295c9d8d5dbc
SAYEED <sayeed.705@gmail.com>
2020-12-15 15:46:38 +0530

    added createProduct and upadteProduct Endpoints

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,13 +4,19 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 //path to all products
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 
 //path to single product by id
-router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
