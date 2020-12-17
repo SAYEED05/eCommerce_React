f3554fabb9c396f8666fb18f2be1bc2b9ce431e2
SAYEED <sayeed.705@gmail.com>
2020-12-17 17:11:30 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -32,13 +32,13 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
+
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
-  //root path
   app.get("/", (req, res) => {
-    res.send("server is running now");
+    res.send("API is running....");
   });
 }
 
