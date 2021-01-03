85e3f49689a571235d781935800c83abf832b782
SAYEED <sayeed.705@gmail.com>
2021-01-04 00:48:13 +0530

    consequent orders without refresh bug fixed

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -2,6 +2,7 @@ import {
   ORDER_CREATE_FAIL,
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_SUCCESS,
+  ORDER_CREATE_RESET,
   ORDER_DELIVER_FAIL,
   ORDER_DELIVER_REQUEST,
   ORDER_DELIVER_SUCCESS,
@@ -46,6 +47,9 @@ export const orderCreateReducer = (state = {}, action) => {
         loading: false,
         error: action.payload,
       };
+
+    case ORDER_CREATE_RESET:
+      return {};
     default:
       return state;
   }
